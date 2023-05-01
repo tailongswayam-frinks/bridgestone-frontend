81705f914218af4526f96f655cc62914ed7c5e70
Akshay Chaturvedi <akshay.chaturvedi@frinks.ai>
2023-05-01 12:39:11 +0530

    adds refetch timeout

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,3 @@
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || '';
 export const CLIENT_NAME = process.env.NEXT_PUBLIC_CLIENT_NAME;
+export const SYSTEM_REFETCH_TIMEOUT_MS = process.env.NEXT_PUBLIC_SYSTEM_REFETCH_TIMEOUT_MS;
