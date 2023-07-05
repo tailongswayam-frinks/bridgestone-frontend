ce4f0ea1aaceb5b112c487904a44f5905cb4aa6c
mukulfrinks <mukul.yadav@frinks.ai>
2023-07-05 17:24:04 +0530

    completed dispatch report

--- components/Summary.js
+++ components/Summary.js
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react';
 import Container from 'styles/summary.styles';
-import {
-  Grid, Select, ButtonGroup, Button, MenuItem,
-} from '@material-ui/core';
+import { Grid, Select, ButtonGroup, Button, MenuItem } from '@material-ui/core';
 import { get, getFile } from 'utils/api';
 import Layout from 'components/Layout';
 import Maintenance from 'components/Maintenance';
@@ -11,7 +9,7 @@ import {
   LocalizationProvider,
   DateCalendar,
   DatePicker,
-  DesktopDatePicker,
+  DesktopDatePicker
 } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { makeStyles } from '@material-ui/core/styles';
@@ -23,7 +21,7 @@ import SummaryChart from './SummaryChart';
 import SummaryAnalysis from './SummaryAnalysis';
 import SummaryLoaderAnalysis from './SummaryLoaderAnalysis';
 
-const downloadPDF = (file) => {
+const downloadPDF = file => {
   const downloadLink = document.createElement('a');
   const fileName = 'report.xlsx';
   downloadLink.setAttribute('download', fileName);
@@ -33,32 +31,32 @@ const downloadPDF = (file) => {
   downloadLink.remove();
 };
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   select: {
     width: '300px',
     [theme.breakpoints.up(1550)]: {
       width: '450px',
       marginRight: '150px',
-      marginLeft: '-40px',
+      marginLeft: '-40px'
     },
     [theme.breakpoints.down(1550)]: {
       // width: '450px',
-      marginRight: 'auto',
-    },
+      marginRight: 'auto'
+    }
   },
   select_1: {
     width: '200px',
     [theme.breakpoints.up(1550)]: {
       width: '250px',
-      marginRight: '-30px',
-    },
+      marginRight: '-30px'
+    }
   },
   select_2: {
     width: '100px',
     [theme.breakpoints.up(1550)]: {
-      width: '125px',
-    },
-  },
+      width: '125px'
+    }
+  }
 }));
 
 function Summary() {
@@ -111,18 +109,19 @@ function Summary() {
       const dateObj = new Date(time);
       const newDateRange = [
         dateObj.setUTCHours(18, 30, 0, 999),
-        dateObj.setUTCHours(41, 89, 59, 999),
+        dateObj.setUTCHours(41, 89, 59, 999)
       ];
 
       const newUpdatedDateRange = [
         newDateRange[0] + 86400000,
-        newDateRange[1] + 86400000,
+        newDateRange[1] + 86400000
       ];
+      console.log(newDateRange, newUpdatedDateRange);
       const data = await get('/api/stats/summarized-stats', {
         // dateRange: getStartAndEndDate()
         dateRange: newDateRange,
         shift: shiftType,
-        updatedDateRange: newUpdatedDateRange,
+        updatedDateRange: newUpdatedDateRange
       });
       setSummaryData(data?.data?.data?.analysis);
       setShiftCount(data?.data?.data?.shift);
@@ -135,8 +134,20 @@ function Summary() {
   }, [time, shiftType]);
 
   const handleDownload = async () => {
+    const dateObj = new Date(time);
+    const newDateRange = [
+      dateObj.setUTCHours(18, 30, 0, 999),
+      dateObj.setUTCHours(41, 89, 59, 999)
+    ];
+
+    const newUpdatedDateRange = [
+      newDateRange[0] + 86400000,
+      newDateRange[1] + 86400000
+    ];
+    console.log(newDateRange, newUpdatedDateRange);
     const res = await getFile('/api/report/datewise', {
-      date: time,
+      dateRange: newDateRange,
+      updatedDateRange: newUpdatedDateRange
     });
     downloadPDF(res.data);
   };
@@ -173,7 +184,7 @@ function Summary() {
             justifyContent: 'space-around',
             // height: '40px',
             marginTop: '5px',
-            alignItems: 'center',
+            alignItems: 'center'
           }}
         >
           <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -181,7 +192,7 @@ function Summary() {
               <Select
                 value={filter}
                 className={classes.select}
-                onChange={(e) => setFilter(e.target.value)}
+                onChange={e => setFilter(e.target.value)}
                 style={{
                   fontSize: '28px',
                   fontWeight: '600',
@@ -190,7 +201,7 @@ function Summary() {
                   outlineColor: '#3A0CA3',
                   border: '1px solid #3A0CA3',
                   // width: '450px',
-                  padding: '0px',
+                  padding: '0px'
                   // marginLeft: '-40px'
                 }}
                 variant="outlined"
@@ -212,7 +223,7 @@ function Summary() {
                   backgroundColor: bagType === 0 ? '#B5179E' : '#F5F5F5',
                   color: bagType === 1 ? '#B5179E' : '#F5F5F5',
                   // width: '120px',
-                  height: '50px',
+                  height: '50px'
                 }}
                 onClick={() => {
                   setBagType(0);
@@ -226,7 +237,7 @@ function Summary() {
                   backgroundColor: bagType === 1 ? '#B5179E' : '#F5F5F5',
                   color: bagType === 0 ? '#B5179E' : '#F5F5F5',
                   // width: '120px',
-                  height: '50px',
+                  height: '50px'
                 }}
                 onClick={() => {
                   setBagType(1);
@@ -278,7 +289,7 @@ function Summary() {
               <DesktopDatePicker
                 format="DD MMM, YYYY"
                 value={time}
-                onChange={(item) => {
+                onChange={item => {
                   console.log(time);
                   // setTime(item?.$d.toLocaleDateString('en-US', options));
 
@@ -292,10 +303,10 @@ function Summary() {
               <Select
                 className={classes.select_1}
                 value={shiftType}
-                onChange={(e) => setShiftType(e.target.value)}
+                onChange={e => setShiftType(e.target.value)}
                 style={{
                   fontSize: '14px',
-                  background: 'white',
+                  background: 'white'
                   // width: '240px',
                   // marginRight: '-20px'
                 }}
@@ -327,7 +338,7 @@ function Summary() {
               className="count-container"
               style={{
                 // marginLeft: '40px',
-                marginTop: '20px',
+                marginTop: '20px'
                 // marginRight: '40px'
               }}
             >
@@ -344,8 +355,8 @@ function Summary() {
                             ? `${summaryData?.total_bags_packed} Bags`
                             : `${summaryData?.total_bags_packed / 20} Tones`
                           : bagType === 0
-                            ? `${summaryData?.total_bags_dispatched} Bags`
-                            : `${summaryData?.total_bags_dispatched / 20} Tones`
+                          ? `${summaryData?.total_bags_dispatched} Bags`
+                          : `${summaryData?.total_bags_dispatched / 20} Tones`
                         : 'NA'}
                     </p>
                     <p className="description">
@@ -365,8 +376,8 @@ function Summary() {
                             ? `${summaryData?.total_missed_labels} Bags`
                             : `${summaryData?.total_missed_labels / 20} Tones`
                           : bagType === 0
-                            ? `${summaryData?.total_burstage_count} Bags`
-                            : `${summaryData?.total_burstage_count / 20} Tones`
+                          ? `${summaryData?.total_burstage_count} Bags`
+                          : `${summaryData?.total_burstage_count / 20} Tones`
                         : 'NA'}
                     </p>
                     <p className="description">
@@ -396,7 +407,7 @@ function Summary() {
                 style={{
                   background: 'white',
                   alignItems: 'center',
-                  justifyContent: 'center',
+                  justifyContent: 'center'
                 }}
               >
                 <SummaryChart
@@ -424,13 +435,13 @@ function Summary() {
                   maxHeight: '58vh',
                   background: 'white',
                   overflowY: 'auto',
-                  overflowX: 'hidden',
+                  overflowX: 'hidden'
                 }}
               >
                 <div className="count-container">
-                  {!filter
-                    && summaryData?.packerBags
-                    && Object.keys(summaryData?.packerBags)?.map((key) => {
+                  {!filter &&
+                    summaryData?.packerBags &&
+                    Object.keys(summaryData?.packerBags)?.map(key => {
                       const value = summaryData?.packerBags[key];
 
                       return (
@@ -444,8 +455,8 @@ function Summary() {
                     })}
                   {filter ? (
                     <Grid container spacing={3}>
-                      {summaryData?.loaderBags
-                        && Object.keys(summaryData?.loaderBags)?.map((key) => {
+                      {summaryData?.loaderBags &&
+                        Object.keys(summaryData?.loaderBags)?.map(key => {
                           const value = summaryData?.loaderBags[key];
                           return (
                             <SummaryLoaderAnalysis
