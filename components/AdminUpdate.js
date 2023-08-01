d863ee65c3aae72c1d080c7b81dc964331eeb329
mukulfrinks <mukul.yadav@frinks.ai>
2023-08-01 17:36:06 +0530

    created imshow toggle

--- components/AdminUpdate.js
+++ components/AdminUpdate.js
@@ -9,17 +9,68 @@ import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
+  Button,
+  ButtonGroup
 } from '@material-ui/core';
 import UpdateDatabase from 'components/UpdateDatabase';
 import UpdateModelWeights from 'components/UpdateModelWeights';
 import PythonDataExtraction from 'components/PythonDataExtraction';
-import { get } from 'utils/api';
+import { get, post } from 'utils/api';
 import UpdateTmate from 'components/UpdateTmate';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  select: {
+    width: '300px',
+    [theme.breakpoints.up(1550)]: {
+      width: '450px',
+      marginRight: '150px',
+      marginLeft: '-40px'
+    },
+    [theme.breakpoints.down(1550)]: {
+      // width: '450px',
+      marginRight: 'auto'
+    }
+  },
+  select_1: {
+    width: '200px',
+    [theme.breakpoints.up(1550)]: {
+      width: '250px',
+      marginRight: '-30px'
+    }
+  },
+  select_2: {
+    width: '100px',
+    [theme.breakpoints.up(1550)]: {
+      width: '125px'
+    }
+  },
+  toggle: {
+    margin: '50px'
+  }
+}));
 
 function Admin() {
   const router = useRouter();
+  const classes = useStyles();
+
   const { userData } = useContext(GlobalContext);
   const [dataExtractionStatus, setDataExtractionStatus] = useState(false);
+  const [imShow, setImShow] = useState(0);
+
+  const handleImShowOff = async () => {
+    setImShow(0);
+    await post('api/configuration/toggle-imshow', {
+      imShow: 0
+    });
+  };
+
+  const handleImShowOn = async () => {
+    setImShow(1);
+    await post('api/configuration/toggle-imshow', {
+      imShow: 1
+    });
+  };
 
   useEffect(() => {
     const fetchStatus = async () => {
@@ -43,6 +94,50 @@ function Admin() {
       <Container>
         <Accordion>
           <AccordionSummary expandIcon={<MdOutlineExpandMore />}>
+            Real Time Video
+          </AccordionSummary>
+          <AccordionDetails>
+            <div className="center">
+              {/* <p className="update-scope">
+                Updates database values by fetching info from AWS and restarts
+                necessary modules
+              </p> */}
+              <ButtonGroup
+                // className={classes.select_1}
+                variant="contained"
+                aria-label="outlined primary button group"
+                // className={classes.toggle}
+              >
+                <Button
+                  className={classes.select_2}
+                  style={{
+                    backgroundColor: imShow === 0 ? '#B5179E' : '#F5F5F5',
+                    color: imShow === 1 ? '#B5179E' : '#F5F5F5',
+                    // width: '120px',
+                    height: '50px'
+                  }}
+                  onClick={handleImShowOff}
+                >
+                  On
+                </Button>
+                <Button
+                  className={classes.select_2}
+                  style={{
+                    backgroundColor: imShow === 1 ? '#B5179E' : '#F5F5F5',
+                    color: imShow === 0 ? '#B5179E' : '#F5F5F5',
+                    // width: '120px',
+                    height: '50px'
+                  }}
+                  onClick={handleImShowOn}
+                >
+                  Off
+                </Button>
+              </ButtonGroup>
+            </div>
+          </AccordionDetails>
+        </Accordion>{' '}
+        <Accordion>
+          <AccordionSummary expandIcon={<MdOutlineExpandMore />}>
             Update Database
           </AccordionSummary>
           <AccordionDetails>
@@ -64,7 +159,7 @@ function Admin() {
           <AccordionDetails>
             <PythonDataExtraction
               dataExtractionStatus={dataExtractionStatus}
-              setDataExtractionStatus={(e) => setDataExtractionStatus(e)}
+              setDataExtractionStatus={e => setDataExtractionStatus(e)}
             />
           </AccordionDetails>
         </Accordion>
