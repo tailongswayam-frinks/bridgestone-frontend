c9f10d483a1436981445df8d0686f91cd42d414d
mukulfrinks <mukul.yadav@frinks.ai>
2023-08-10 17:37:38 +0530

    completed rack status

--- components/ShipmentAnalysis.js
+++ components/ShipmentAnalysis.js
@@ -4,6 +4,7 @@ import { setLocalStorage, getLocalStorage } from 'utils/storage';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import LoaderAnalysisRow from './LoaderAnalysisRow';
+import { get } from 'utils/api';
 
 // Define styles using makeStyles
 const useStyles = makeStyles(() => ({
@@ -11,14 +12,14 @@ const useStyles = makeStyles(() => ({
     marginTop: '120px',
     marginLeft: '80px',
     fontSize: '24px',
-    fontWeight: '800',
+    fontWeight: '800'
   },
   rackContainer1: {
     marginTop: '120px',
     marginLeft: '80px',
     fontSize: '24px',
     fontWeight: '800',
-    height: '20px',
+    height: '20px'
   },
   inputRackNo: {
     marginTop: '20px',
@@ -29,7 +30,7 @@ const useStyles = makeStyles(() => ({
     borderRadius: '6px',
     width: '130px',
     border: 'none',
-    padding: '0 10px',
+    padding: '0 10px'
   },
   rackButton: {
     backgroundColor: '#008847',
@@ -38,7 +39,7 @@ const useStyles = makeStyles(() => ({
     // padding: '2px 9px',
     fontWeight: '700',
     border: 'none',
-    height: '30px',
+    height: '30px'
   },
   editRackButton: {
     backgroundColor: '#69E866',
@@ -47,18 +48,18 @@ const useStyles = makeStyles(() => ({
     // padding: '2px 9px',
     fontWeight: '700',
     border: 'none',
-    height: '30px',
+    height: '30px'
   },
   tableContainer: {
-    marginTop: '140px',
+    marginTop: '140px'
     // marginLeft: '80px',
     // fontSize: '24px',
     // fontWeight: '800'
   },
   tableDiv: {
     // padding: '0px',
-    paddingBottom: '30px',
-  },
+    paddingBottom: '30px'
+  }
 }));
 
 function ShipmentAnalysis({
@@ -68,7 +69,7 @@ function ShipmentAnalysis({
   handleFlag,
   handleBagDone,
   handleBagIncrement,
-  handleBeltReset,
+  handleBeltReset
 }) {
   const classes = useStyles();
   const { bagTypes: BAG_TYPES } = useContext(GlobalContext);
@@ -77,6 +78,7 @@ function ShipmentAnalysis({
   const [rackNo, setRackNo] = useState('');
   const [rackNoModified, setRackNoModified] = useState(false);
   const [savedRackNo, setSavedRackNo] = useState('');
+  const [rackStarted, setRackStarted] = useState(false);
 
   useEffect(() => {
     setFilterButton(vehicleType);
@@ -85,7 +87,7 @@ function ShipmentAnalysis({
   useEffect(() => {
     const filtertedLoaders = {};
     if (vehicleBelts) {
-      Object.values(vehicleBelts).forEach((element) => {
+      Object.values(vehicleBelts).forEach(element => {
         if (element.vehicle_type === filterButton) {
           filtertedLoaders[element.vehicle_id] = element;
         }
@@ -95,76 +97,85 @@ function ShipmentAnalysis({
   }, [vehicleBelts, filterButton]);
 
   useEffect(() => {
-    setSavedRackNo(getLocalStorage('rackno'));
-    setRackNo(getLocalStorage('rackno'));
+    // setSavedRackNo(getLocalStorage('rackno'));
+    // setRackNo(getLocalStorage('rackno'));
+    const fetchRackStatus = async () => {
+      const res = await get('/api/shipment/rack-status');
+      setRackStarted(res?.data?.rackStatus);
+    };
+    fetchRackStatus();
   }, []);
 
   return (
     <>
       {vehicleType === 1 && (
         <div className={classes.rackContainer}>
-          RACK NUMBER :
-          {' '}
+          RACK NUMBER :{' '}
           <input
             className={classes.inputRackNo}
             placeholder="Rack No."
-            onChange={(e) => {
+            onChange={e => {
               setRackNo(e.target.value);
               setRackNoModified(true);
             }}
             value={rackNo}
+            disabled={rackStarted}
           />
-          {rackNoModified ? (
-            <Button
-              className={classes.editRackButton}
-              onClick={() => {
-                setRackNoModified(false);
-                setLocalStorage('rackno', rackNo);
-                setSavedRackNo(rackNo);
-              }}
-            >
-              SAVE
-            </Button>
-          ) : null}
+          <Button
+            className={classes.editRackButton}
+            onClick={() => {
+              setRackNoModified(false);
+              setLocalStorage('rackno', rackNo);
+              setSavedRackNo(rackNo);
+              setRackStarted(prev => !prev);
+            }}
+          >
+            {rackStarted ? 'END' : 'START'}
+          </Button>
         </div>
       )}
       {vehicleType === 0 && <div className={classes.rackContainer1} />}
-      <div className={classes.tableDiv}>
-        <table className="custom-table">
-          <thead>
-            <tr>
-              <th>S.No.</th>
-              <th>LODNO</th>
-              <th>GRADE</th>
-              <th>{vehicleType === 1 ? 'WAGON NO' : 'TRUCK NO'}</th>
-              <th>TARGET</th>
-              <th>ACTUAL</th>
-              <th>ADD BAG</th>
-              <th>START TIME</th>
-              <th>SET</th>
-              <th>VIEW</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filterVehicle
-              && Object.values(filterVehicle).map((e, index) => (
-                <LoaderAnalysisRow
-                  key={index}
-                  data={e}
-                  BAG_TYPES={BAG_TYPES}
-                  handleNewShipment={(arg) => handleNewShipment(arg)}
-                  handleFlag={handleFlag}
-                  index={index + 1}
-                  rackNo={savedRackNo}
-                  vehicleType={vehicleType}
-                  handleBagDone={handleBagDone}
-                  handleBagIncrement={handleBagIncrement}
-                  handleBeltReset={handleBeltReset}
-                />
-              ))}
-          </tbody>
-        </table>
-      </div>
+      {(rackStarted || vehicleType === 0) && (
+        <div className={classes.tableDiv}>
+          <table className="custom-table">
+            <thead>
+              <tr>
+                <th>S.No.</th>
+                <th>LODNO</th>
+                <th>GRADE</th>
+                <th>{vehicleType === 1 ? 'WAGON NO' : 'TRUCK NO'}</th>
+                <th>TARGET</th>
+                <th>ACTUAL</th>
+                <th>ADD BAG</th>
+                <th>START TIME</th>
+                <th>SET</th>
+                <th>VIEW</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filterVehicle &&
+                Object.values(filterVehicle).map((e, index) => (
+                  <LoaderAnalysisRow
+                    key={index}
+                    data={e}
+                    BAG_TYPES={BAG_TYPES}
+                    handleNewShipment={arg => handleNewShipment(arg)}
+                    handleFlag={handleFlag}
+                    index={index + 1}
+                    rackNo={savedRackNo}
+                    vehicleType={vehicleType}
+                    handleBagDone={handleBagDone}
+                    handleBagIncrement={handleBagIncrement}
+                    handleBeltReset={handleBeltReset}
+                  />
+                ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+      {!rackStarted && vehicleType === 1 && (
+        <h1 style={{ marginTop: '100px' }}>Please start the rack first</h1>
+      )}
     </>
   );
 }
