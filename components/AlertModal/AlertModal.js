29594d8a30c23ef680bd5782bfd22ce47b30f169
mukulfrinks <mukul.yadav@frinks.ai>
2023-11-18 13:28:46 +0530

    completed health-backend

--- components/AlertModal/AlertModal.js
+++ components/AlertModal/AlertModal.js
@@ -1,17 +1,20 @@
 import { makeStyles } from '@material-ui/core/styles';
-import { Modal, Backdrop, Fade, Button } from '@material-ui/core';
+import {
+  Modal, Backdrop, Fade, Button,
+} from '@material-ui/core';
 import PropTypes from 'prop-types';
 import Image from 'next/image';
 import theme from 'styles/theme';
 
 import ImageKitLoader from 'utils/ImageLoader';
+import { useEffect, useState } from 'react';
 
 const useStyles = makeStyles(() => ({
   modal: {
     display: 'flex',
     padding: '10px',
     alignItems: 'flex-start',
-    justifyContent: 'center'
+    justifyContent: 'center',
     // height: '80vh'
   },
   paper: {
@@ -25,19 +28,19 @@ const useStyles = makeStyles(() => ({
     justifyContent: 'space-between',
     alignItems: 'center',
     borderRadius: '0.625em',
-    marginTop: '3.75em'
+    marginTop: '3.75em',
   },
   close: {
     top: '10px',
     right: '10px',
     cursor: 'pointer',
-    position: 'absolute'
+    position: 'absolute',
   },
   imgContainer: {
     textAlign: 'center',
     '&:img': {
-      width: '60px'
-    }
+      width: '60px',
+    },
   },
   button: {
     textTransform: 'capitalize',
@@ -46,23 +49,23 @@ const useStyles = makeStyles(() => ({
     color: 'white',
     background: 'yellow',
     '&:hover': {
-      background: 'pink'
-    }
+      background: 'pink',
+    },
   },
   title: {
     display: 'flex',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   heading: {
     color: 'white',
-    marginLeft: '2.5em'
+    marginLeft: '2.5em',
   },
   bold: {
-    fontWeight: '900'
+    fontWeight: '900',
   },
   subHeading: {
     marginTop: '1.25em',
-    fontSize: '0.8333333333333334vw'
+    fontSize: '0.8333333333333334vw',
   },
   actionButton: {
     background: 'white',
@@ -75,17 +78,42 @@ const useStyles = makeStyles(() => ({
 
     '&:hover': {
       background: theme.palette.gradient.pink,
-      color: 'white'
-    }
+      color: 'white',
+    },
   },
   h4: {
     fontSize: '1.4583333333333333vw',
-    lineHeight: '1.71875vw'
-  }
+    lineHeight: '1.71875vw',
+    marginTop: '20px',
+  },
 }));
 
-function AlertModal({ open, close, alertsnooze }) {
+function AlertModal({
+  open, close, alertsnooze, name,
+}) {
   const classes = useStyles();
+  const [fileContent, setFileContent] = useState('');
+
+  useEffect(() => {
+    // Function to fetch the file content
+    const fetchFileContent = async () => {
+      try {
+        const response = await fetch(
+          '/state_change_logs/logs.txt',
+          // '../../../cement-health-backend/state_change_logs/loading_TX_15.txt'
+        );
+        const content = await response.text();
+        console.log(content, 'content');
+        setFileContent(content);
+        // setModalIsOpen(true);
+      } catch (error) {
+        console.error('Error reading file:', error);
+      }
+    };
+
+    // Call the function
+    fetchFileContent();
+  }, []);
 
   return (
     <Modal
@@ -95,55 +123,62 @@ function AlertModal({ open, close, alertsnooze }) {
       className={classes.modal}
       BackdropComponent={Backdrop}
       BackdropProps={{
-        timeout: 500
+        timeout: 500,
       }}
     >
       <Fade in={open}>
         <div className={classes.paper}>
           <div className={classes.title}>
-            <div className="icon"></div>
+            <div className="icon" />
             <div className={classes.heading}>
-              <h4 className={classes.h4}>
-                Loader #6: <span className={classes.bold}>Incorrect bags</span>
-              </h4>
-              <p className={classes.subHeading}>
-                Here is a subtitle for this table
-              </p>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                }}
+              >
+                <h4 className={classes.h4}>
+                  State change logs
+                  {/* <span className={classes.bold}></span> */}
+                </h4>
+                <Button
+                  variant="contained"
+                  className={classes.actionButton}
+                  onClick={close}
+                >
+                  Close
+                </Button>
+              </div>
+              <p className={classes.subHeading}>{fileContent}</p>
             </div>
           </div>
           <div>
             {/* <Button variant="contained" className={classes.actionButton}>
               Mark Solved
             </Button> */}
-            <Button
-              variant="contained"
-              className={classes.actionButton}
-              onClick={alertsnooze}
-            >
-              Snooze
-            </Button>
           </div>
         </div>
       </Fade>
     </Modal>
 
-    // <MuiAlert
-    //   className={classes.alert}
-    //   open={open}
-    //   // onClose={() => handleClose(alert)}
-    //   id={alert.id}
-    //   elevation={6}
-    //   variant="filled"
-    //   severity={alert.type}
-    // >
-    //   <AlertTitle>hello</AlertTitle>
-    //   alert
-    // </MuiAlert>
+  // <MuiAlert
+  //   className={classes.alert}
+  //   open={open}
+  //   // onClose={() => handleClose(alert)}
+  //   id={alert.id}
+  //   elevation={6}
+  //   variant="filled"
+  //   severity={alert.type}
+  // >
+  //   <AlertTitle>hello</AlertTitle>
+  //   alert
+  // </MuiAlert>
   );
 }
 
 AlertModal.propTypes = {
   open: PropTypes.bool.isRequired,
-  close: PropTypes.func.isRequired
+  close: PropTypes.func.isRequired,
 };
 export default AlertModal;
